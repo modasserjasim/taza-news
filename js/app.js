c3f9acca149a1055e2715d9fe0adaaa4ec69ee96
Modasserul Hoque <modasserulhaq.jasim@gmail.com>
2022-09-03 04:01:49 +0600

    card information added

--- js/app.js
+++ js/app.js
@@ -38,33 +38,27 @@ const displayCategoriesNews = newsCard => {
             <div class="card shadow-sm border-0 rounded-4 mb-4">
                 <div class="row p-3 d-flex align-items-center">
                     <div class="col-md-3">
-                        <img src="images/thumb.jpg" class="card-img" alt="...">
+                        <img src="${news.thumbnail_url}" class="card-img" alt="...">
                     </div>
                     <div class="col-md-9">
                         <div class="card-body">
-                            <h4 class="card-title">The best fashion influencers to follow for sartorial inspiration</h4>
+                            <h4 class="card-title">${news.title}</h4>
                             <div class="d-flex align-items-center gap-3 py-3">
 
                                 <div class="d-flex align-items-center gap-2">
-                                    <img src="images/Rectangle 19.png" style="margin-top: -10px;" alt="author">
-                                    <p>Jane Cooper</p>
+                                    <img class="rounded-pill" src="${news.author.img}" style="margin-top: -10px; width:40px; height: 40px;" alt="author">
+                                    <p>${news.author.name}</p>
                                 </div>
                                 <div>
-                                    <p class=""><i class="fa-sharp fa-solid fa-calendar-days me-1"></i> Jan 10, 2022</p>
+                                    <p class=""><i class="fa-sharp fa-solid fa-calendar-days me-1"></i> ${news.author.published_date}</p>
                                 </div>
                             </div>
-                            <p class="card-text text-secondary">From our favourite UK influencers to the best missives
-                                from Milan and the coolest New Yorkers, read on some of the best fashion blogs out
-                                there, and for even more inspiration, do head to our separate black fashion influencer
-                                round-up. <br> <br>
-
-                                Fancy some shopping deals? Check out these amazing sales: Zara Black Friday, ASOS Black
-                                Friday, Missoma Black Friday and Gucci Black Friday...</p>
+                            <p class="card-text text-secondary">${news.details}</p>
                             <div>
                                 <ul class="d-flex justify-content-between list-unstyled pt-3">
                                     <li><i class="fas fa-tag me-1"></i>  Category</li>
-                                    <li><i class="fa-regular fa-eye me-1"></i>  1.5M</li>
-                                    <li >Rating <i class="fa-solid fa-star ms-1"></i></li>
+                                    <li><i class="fa-regular fa-eye me-1"></i>  ${news.total_view}</li>
+                                    <li >${news.rating.number} <i class="fa-solid fa-star ms-1"></i></li>
                                     <li class="text-info" role="button"><i class="fa fa-arrow-right"></i>  Continue Reading</li>
                                 </ul>
                             </div>
