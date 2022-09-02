ebef9acf2e30f05c84fe9811b5b18bf1830d715f
Modasserul Hoque <modasserulhaq.jasim@gmail.com>
2022-09-03 03:12:20 +0600

    Card design completed

--- js/app.js
+++ js/app.js
@@ -7,14 +7,35 @@ const loadAllCategories = async() =>{
 }
 const displayCategories = async() =>{
     const categories = await loadAllCategories();
-    console.log(categories);
+    // console.log(categories);
     const categoriesMenuContainer = document.getElementById('categories-menu');
     categories.forEach(category => {
         // console.log(category);
-        const newButton = document.createElement('a');
+        const newButton = document.createElement('div');
         newButton.innerHTML = `
-        <button class="category-btn px-4 mx-2 text-secondary">${category.category_name}</button>`;
+        <button onclick="loadCategoriesNews('${category.category_id}')" class="category-btn text-secondary">${category.category_name}</button>`;
         categoriesMenuContainer.appendChild(newButton);
     });
 }
 displayCategories();
+
+const loadCategoriesNews = async(category_id) =>{
+    console.log(category_id);
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategoriesNews(data.data);
+}
+
+const displayCategoriesNews = newsCard =>{
+    console.log(newsCard);
+    const categoriesContainer = document.getElementById('categories-container');
+    newsCard.forEach(news => {
+        console.log(news);
+        const createCard = document.createElement('div');
+        createCard.classList.add('card shadow-sm border-0 rounded-4 mb-4');
+        createCard.innerHTML = `
+        
+        `;
+    });
+}
